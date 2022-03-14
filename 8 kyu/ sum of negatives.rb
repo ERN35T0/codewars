7a3d8f451e72d866d5ef61417f4fae2a8ac28b84
ERN35T0 <ernest.baquero@gmail.com>
2022-03-14 05:40:17 +0000

    add first katas

---
+++ 8 kyu/ sum of negatives.rb
@@ -0,0 +1,37 @@
+def count_positives_sum_negatives(lst)
+	sum = 0
+	k = 0
+	if lst == []
+		[]
+	else
+	count_positive = lst.count {|number| number > 0}
+	sum_negative = lst.each {|number| sum += number if number < 0}
+		[count_positive, sum]
+	end
+end
+=begin
+others posibilites
+def count_positives_sum_negatives(lst)
+	return [] if lst.nil? || lst.empty?
+	[lst.count(&:positive?), lst.select(&:negative?).reduce(0, :+)]
+  end
+///////////////////////////////////////////////////////////////////////////
+def count_positives_sum_negatives(lst)
+  return [] if lst.empty?
+
+  positives, negatives = lst.partition(&:positive?)
+
+  [positives.count, negatives.sum]
+end
+///////////////////////////////////////////////////////////////////////////
+def count_positives_sum_negatives(lst)
+  lst.size == 0 ? [] : [lst.select {|x| x > 0}.size , lst.select {|x| x < 0}.sum ]
+end
+////////////////////////////////////////////////////////////////////////////
+def count_positives_sum_negatives(lst)
+  #your code here
+  return [] if lst.empty?
+  [lst.count{|n| n > 0}, lst.select{|n| n < 0}.sum]
+end
+=end
+
---
+++ 8 kyu/Are You Playing Banjo?.rb
@@ -0,0 +1,4 @@
+def are_you_playing_banjo(name)
+first = name[0].upcase
+first == "R" ? "#{name} plays banjo" : "#{name} does not play banjo"
+end
