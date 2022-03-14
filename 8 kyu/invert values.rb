e86c10d7fcab5db68c674951f4afb09c3eebcb44
ERN35T0 <ernest.baquero@gmail.com>
2022-03-14 15:11:26 +0000

    invert values

---
+++ 8 kyu/invert values.rb
@@ -0,0 +1,24 @@
+invert values. rb
+def invert(list)
+list.map { |i| -i }
+end
+=begin
+others solutions
+
+def invert(list)
+  list.map {|n| n*-1}
+end
+////////////////////////////
+def invert(list)
+  list.map(&:-@)
+end
+////////////////////////////
+def invert(list)
+  newList = []
+  list.each do |number|
+    inverse = number - (number * 2)
+    newList.push(inverse)
+  end
+  newList
+end
+=end
